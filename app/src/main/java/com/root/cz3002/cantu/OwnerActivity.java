831ee10507d275055cb1864ed73aa7560581fe99
9711chris <christantia@live.com>
2017-10-21 04:24:21 +0800

    modified owner's orders

--- app/src/main/java/com/root/cz3002/cantu/OwnerActivity.java
+++ app/src/main/java/com/root/cz3002/cantu/OwnerActivity.java
@@ -11,10 +11,18 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.root.cz3002.cantu.model.Order;
+import com.root.cz3002.cantu.model.SOChildData;
+import com.root.cz3002.cantu.model.SOHeaderData;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class OwnerActivity extends AppCompatActivity {
 
@@ -22,6 +30,10 @@ public class OwnerActivity extends AppCompatActivity {
     //private String mode;
     private LinearLayout list;
     //private ArrayList<Stall> groupOrders = new ArrayList<Stall>(); //to fill with content of list for stalls in canteen/cuisine choice
+    SOExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<SOHeaderData> listDataHeader;
+    HashMap<String, List<SOChildData>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,54 +58,77 @@ public class OwnerActivity extends AppCompatActivity {
         final Context context = this;
         final Bundle bundle = new Bundle();
 
-        FloatingActionButton switchMode = (FloatingActionButton) findViewById(R.id.switchMode);
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.e_list);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new SOExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+       /* FloatingActionButton switchMode = (FloatingActionButton) findViewById(R.id.switchMode);
         LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
         list = (LinearLayout) inflater.inflate(R.layout.list_view, (ViewGroup) findViewById(R.id.list));
 
-        populateOrder(null);
+        populateOrder(null);*/
 
     }
-    private boolean addNewItemInList(final LinearLayout list, final Order cur) {
-        final LinearLayout a = new LinearLayout(this);
-        a.setOrientation(LinearLayout.HORIZONTAL);
 
-        OrderListView view = new OrderListView(this, cur);
-        view.setLayoutParams(new LinearLayout.LayoutParams(Toolbar.LayoutParams.MATCH_PARENT, Toolbar.LayoutParams.WRAP_CONTENT));
-        view.setBackgroundResource(R.drawable.background_border_red);
+    private void prepareListData() {
 
-        a.addView(view);
-        a.setTag(cur);
-        a.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder1 = new AlertDialog.Builder(OwnerActivity.this);
-                builder1.setMessage("Are you sure you want to resolve and delete this order?");
-                builder1.setCancelable(true);
+        listDataHeader = new ArrayList<SOHeaderData>();
+        listDataChild = new HashMap<String, List<SOChildData>>();
 
-                builder1.setPositiveButton(
-                        "Yes",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                list.removeView(a);
-                                //database method to remove order in DB
-                            }
-                        });
+        //================HOW TO ADD THE DATA==============
+        // Adding header data
+        SOHeaderData order1 = new SOHeaderData("Order 1", 5.5);
+        listDataHeader.add(order1);
+        List<SOChildData> dishes1 = new ArrayList<SOChildData>();
+        // Adding child data
+        SOChildData dish1 = new SOChildData("Chicken Rice", 2);
+        dishes1.add(dish1);
+        dishes1.add(dish1);
+        listDataChild.put(listDataHeader.get(0).getOrderSeq(), dishes1); // Header, Child data
 
-                builder1.setNegativeButton(
-                        "No",
-                        new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.cancel();
-                            }
-                        });
+        // Adding header data
+        SOHeaderData order2 = new SOHeaderData("Order 2", 4.5);
+        listDataHeader.add(order2);
+        List<SOChildData> dishes2 = new ArrayList<SOChildData>();
+        // Adding child data
+        SOChildData dish2 = new SOChildData("Chicken Rice2", 3);
+        dishes2.add(dish2);
+        dishes2.add(dish2);
+        listDataChild.put(listDataHeader.get(1).getOrderSeq(), dishes2); // Header, Child data
 
-                AlertDialog alert11 = builder1.create();
-                alert11.show();;
+
+        //===================================================
+
+        //TODO: FETCH FROM DATABASE
+        //====================HOW TO ADD THE DATA WITH LOOP, FOR DATABASE==============
+        /*for (int i = 0; i < numberOfOrders; i++){
+            SOHeaderData order = new SOHeaderData("Order " + orderIDNum, totalprice);
+            listDataHeader.add(order);
+            List<SOChildData> dishesInOrder = new ArrayList<SOChildData>();
+            for (int j = 0; j < numberOfDishesInTheOrder; j++) {
+                SOChildData dish = new SOChildData(dishName, dishQty);
+                dishesInOrder.add(dish);
             }
-        });
+            listDataChild.put(listDataHeader.get(i).getOrderSeq(), dishesInOrder);
+        }*/
 
-        list.addView(a);
-        return false;
+
+
+    }
+
+    public void refresh(View v){
+        Intent intent = getIntent();
+        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        finish();
+        startActivity(intent);
+        overridePendingTransition(0, 0);
     }
 
     public void populateOrder(View v) {
@@ -101,11 +136,6 @@ public class OwnerActivity extends AppCompatActivity {
 
         //dummy, example for populating list
         Order temp = new Order(1,"Sambal Fried Rice",1,3.5,3.5);
-        addNewItemInList(list, temp);
-        addNewItemInList(list, temp);
-        addNewItemInList(list, temp);
-        addNewItemInList(list, temp);
-        addNewItemInList(list, temp);
 
         //populate groupOrders arraylist
         //using Database's method
@@ -121,7 +151,5 @@ public class OwnerActivity extends AppCompatActivity {
        if (v != null)
            Toast.makeText(OwnerActivity.this, "Refreshed", Toast.LENGTH_SHORT).show();
     }
-
-
 
 }
---
+++ app/src/main/java/com/root/cz3002/cantu/SOExpandableListAdapter.java
@@ -0,0 +1,138 @@
+package com.root.cz3002.cantu;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.root.cz3002.cantu.model.SOChildData;
+import com.root.cz3002.cantu.model.SOHeaderData;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Christantia on 10/20/2017.
+ */
+
+public class SOExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context _context;
+    private List<SOHeaderData> _listDataHeader; // header titles
+    // child data in format of header title, child title
+    private HashMap<String, List<SOChildData>> _listDataChild;
+    private RecyclerView.ViewHolder holder;
+
+
+    public SOExpandableListAdapter(Context context, List<SOHeaderData> listDataHeader,
+                                   HashMap<String, List<SOChildData>> listChildData) {
+        this._context = context;
+        this._listDataHeader = listDataHeader;
+        this._listDataChild = listChildData;
+    }
+
+
+    @Override
+    public int getGroupCount() {
+        return this._listDataHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition).getOrderSeq())
+                .size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this._listDataHeader.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return this._listDataChild.get(this._listDataHeader.get(groupPosition).getOrderSeq())
+                .get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(final int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        SOHeaderData headerObj = (SOHeaderData) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this._context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.s_o_listgroup, null);
+
+        }
+
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.order_num);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerObj.getOrderSeq());
+
+        ImageView delete = (ImageView) convertView.findViewById(R.id.delete);
+        delete.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 //TODO: DELETE FROM DATABASE
+                 _listDataChild.remove(_listDataHeader.get(groupPosition).getOrderSeq());
+                 _listDataHeader.remove(_listDataHeader.get(groupPosition));
+                 notifyDataSetChanged();
+                 Toast.makeText(_context, "Deleted", Toast.LENGTH_SHORT).show();
+             }
+         });
+
+        TextView price = (TextView) convertView.findViewById(R.id.total_price);
+        price.setText("Total price: $" + String.valueOf(headerObj.getTotalPrice()));
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+
+            final SOChildData childObj = (SOChildData) getChild(groupPosition, childPosition);
+
+            if (convertView == null) {
+                LayoutInflater infalInflater = (LayoutInflater) this._context
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = infalInflater.inflate(R.layout.s_o_listitem, null);
+            }
+
+            TextView txtListChild1 = (TextView) convertView
+                    .findViewById(R.id.food_name);
+            txtListChild1.setText(childObj.getFoodName());
+
+            TextView txtListChild2 = (TextView) convertView
+                    .findViewById(R.id.qty);
+            txtListChild2.setText("Quantity: " + String.valueOf(childObj.getQty()));
+
+
+            return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
